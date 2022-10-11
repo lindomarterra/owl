e5a12a90bef47c1811cc863fded02969e04d87fa
Lindomar Terra <102319307+lindomarterra@users.noreply.github.com>
2022-10-11 11:08:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+ 
+
+ // PAGE 1
+
+ function burguer(){
+
+    document.getElementById('burguer').style.display = 'none'
+    document.getElementById('letterX').style.display = 'block'
+    }
+    
+    function letter(){
+    
+    document.getElementById('burguer').style.display = 'block'
+    document.getElementById('letterX').style.display = 'none'
+    }
+
+
+
+    
+
+   
+    
+
+
+
+
+
+
+
+      
